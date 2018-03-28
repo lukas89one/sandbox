2f3910865e334e56aa787742cfbe2d8bbb669a16
Denys Chura <chdenan@gmail.com>
2018-03-28 18:28:08 +0300

    SDO-557 Adding Test Snippets api created from DRF tutorial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
